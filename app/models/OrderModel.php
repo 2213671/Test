c3a15d2029758033ce74fff37eef81d8f143eb60
HMT <trong.huynhminh@hcmut.edu.vn>
2026-06-05 12:54:06 +0700

    Delete app directory

--- app/models/OrderModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class OrderModel extends DB {
-    public function getByUserId($uid) {
-        return $this->all("SELECT * FROM orders WHERE user_id=:uid ORDER BY created_at DESC", ['uid'=>$uid]);
-    }
-    public function getItems($orderId) {
-        return $this->all("SELECT * FROM order_items WHERE order_id=:oid", ['oid'=>$orderId]);
-    }
-}
